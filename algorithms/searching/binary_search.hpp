ddd465d9ee92576a70aba128ce8f446cf2926c92
santiagoravila <santyrojas.a@gmail.com>
2025-09-10 16:39:48 -0500

    chore: add initial repo structure with placeholder files for binary_search

---
+++ algorithms/searching/binary_search.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+
+template <typename T>
+bool binary_search(const std::vector<int>& arr, T target)
+{
+    ;
+}
---
+++ tests/test_binary_search.cpp
@@ -0,0 +1,7 @@
+#include "searching/binary_search.hpp"
+#include <vector>
+
+int main()
+{
+    binary_search({1,2,3,4,5}, 5);
+}
